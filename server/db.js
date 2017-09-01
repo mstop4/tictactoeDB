26ae87e6f6f0ab4a989310820a04e9d010c1b2b8
mstop4 <mstop4@gmail.com>
2017-08-31 21:04:12 -0400

    first model

--- server/db.js
+++ server/db.js
@@ -11,4 +11,28 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
   }
 })
 
-module.exports = db
+createTable = () => {
+
+  const Game = db.define('game', {
+    moves: {
+      type: Sequelize.STRING
+    },
+    x_wins: {
+      type: Sequelize.BOOLEAN
+    }
+  })
+
+  Game.sync({force: true})
+    .then(() => {
+    return Game.create({
+      moves: '12634',
+      x_wins: true
+    })
+  })
+
+}
+
+module.exports = {
+  db: db,
+  createTable: createTable
+}
